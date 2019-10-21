aa9dd00f51e598a01e8e4cd6fa7371449346470f
Arthur B'Chir <arthur.bchir@epitech.eu>
2019-10-22 00:13:52 +0200

    return error on non digit arguments

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 #include "tree.h"
+
+static bool isnumber(char *s)
+{
+	while (*s != '\0') {
+		if (!isdigit(*s))
+			return false;
+		s++;
+	}
+
+	return true;
+}
 
 int main(int argc, char **argv)
 {
 	if (argc != 2) {
 		my_put_err("Only one argument required: the size");
+		return 84;
+	}
+
+	if (!isnumber(argv[1])) {
+		my_put_err("Argument should be a digit");
 		return 84;
 	}
 
